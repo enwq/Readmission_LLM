24c51230e95865549f6350d80860ea4d1068ddeb
Yufei Shen <shenyufei@utexas.edu>
2024-04-16 16:23:30 -0500

    Added a working example to get predictions from an ML model

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 # Python file of all tools that can be executed by the agents
 import pandas as pd
 import os
+import pickle
 
 def get_subject_info(file_name: str, subject: str, feature: str)->str:
     """
@@ -13,4 +14,23 @@ def get_subject_info(file_name: str, subject: str, feature: str)->str:
     subject_line = df.loc[df['Name']==subject]
     subject_feature = subject_line[feature].values[0]
     return f"The {feature} for {subject} is {subject_feature}."
+
+def make_price_prediction(file_name: str, subject: str)->str:
+    """
+    Use this function to predict the price of the given subject using a trained machine learning model.
+    file_name is the name of the csv file that contains all data.
+    subject is the name of the subject to make prediction for.
+    """
+    df = pd.read_csv(os.path.join("data",file_name))
+    features = ['AWD','RWD','Engine Size (l)','Cyl','Horsepower(HP)','City Miles Per Gallon','Highway Miles Per Gallon','Weight','Wheel Base','Len','Width']
+    feature_data = df[features]
+    type_dummy = pd.get_dummies(df['Type']).astype(int)
+    feature_data = pd.concat([feature_data,type_dummy],axis=1)
+    subject_data = feature_data.loc[df['Name']==subject]
+    X = subject_data.to_numpy()
+    with open('model.pkl', 'rb') as f:
+        model = pickle.load(f)
+    predicted = model.predict(X)[0]
+    return f"The predicted value for {subject} is {'%.2f' % (predicted)} according to the ML model."
+    
 
