a0aa6bae3f8df4c60a716d69d5ab62e28f4afc9a
Yufei Shen <shenyufei@utexas.edu>
2024-04-16 23:31:42 -0500

    Added a tool to compute change in ML prediction for a change in feature value

--- tools.py
+++ tools.py
@@ -8,19 +8,17 @@ def get_subject_info(file_name: Annotated[str, "The name of the csv file that co
                      subject: Annotated[str, "The name of the subject to extract information for."], 
                      feature: Annotated[str, "The name of the column in the data to extract information for."])->str:
     """
-    Use this function to extract information of a particular feature column for a specific subject from a csv file.
+    A function to extract information of a particular feature column for a specific subject from a csv file.
     """
     df = pd.read_csv(os.path.join("data",file_name))
     subject_line = df.loc[df['Name']==subject]
     subject_feature = subject_line[feature].values[0]
     return f"The {feature} for {subject} is {subject_feature}."
 
-def make_price_prediction(file_name: Annotated[str, "The name of the csv file that contains all data."], 
-                          subject: Annotated[str, "The name of the subject to extract information for."])->str:
+def prepare_model_input(file_name,subject):
     """
-    Use this function to predict the price of the given subject using a trained machine learning model.
-    file_name is the name of the csv file that contains all data.
-    subject is the name of the subject to make prediction for.
+    A helper function to prepare the data from a subject as input to a machine learning model.
+    This function is not registered by the agents.
     """
     df = pd.read_csv(os.path.join("data",file_name))
     features = ['AWD','RWD','Engine Size (l)','Cyl','Horsepower(HP)','City Miles Per Gallon','Highway Miles Per Gallon','Weight','Wheel Base','Len','Width']
@@ -28,6 +26,14 @@ def make_price_prediction(file_name: Annotated[str, "The name of the csv file th
     type_dummy = pd.get_dummies(df['Type']).astype(int)
     feature_data = pd.concat([feature_data,type_dummy],axis=1)
     subject_data = feature_data.loc[df['Name']==subject]
+    return subject_data
+
+def make_price_prediction(file_name: Annotated[str, "The name of the csv file that contains all data."], 
+                          subject: Annotated[str, "The name of the subject to predict price for."])->str:
+    """
+    A function to predict the price of the given subject using a trained machine learning model.
+    """
+    subject_data = prepare_model_input(file_name,subject)
     X = subject_data.to_numpy()
     with open('model.pkl', 'rb') as f:
         model = pickle.load(f)
@@ -35,3 +41,20 @@ def make_price_prediction(file_name: Annotated[str, "The name of the csv file th
     return f"The predicted value for {subject} is {'%.2f' % (predicted)} according to the ML model."
     
 
+def compute_prediction_change(file_name: Annotated[str, "The name of the csv file that contains all data."],
+                              subject: Annotated[str, "The name of the subject to predict price for."],
+                              feature: Annotated[str, "The name of the column in the data that changes."],
+                              change: Annotated[float,"The amount of change for the specific feature column."]):
+    """
+    A function to compute the change in the predicted price of the given subject using a trained machine learning model,
+    when the value of a speific feature is changed by a certain amount.
+    """
+    subject_data = prepare_model_input(file_name,subject)
+    original_feature = subject_data[feature].values[0]
+    with open('model.pkl', 'rb') as f:
+        model = pickle.load(f)
+    original_predicted = model.predict(subject_data.to_numpy())[0]
+    new_feature = original_feature + change
+    subject_data[feature] = new_feature
+    new_predicted = model.predict(subject_data.to_numpy())[0]
+    return f"When the feature {feature} of {subject} changes by {change} from {original_feature} to {new_feature}, the predicted value changes from {'%.2f' % (original_predicted)} to {'%.2f' % (new_predicted)} according to the ML model."
